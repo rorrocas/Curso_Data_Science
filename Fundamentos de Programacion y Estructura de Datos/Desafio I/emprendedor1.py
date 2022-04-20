4782f693262237fd0bd67860045783e4939a960d
Rodrigo Castillo Villegas <96395654+rorrocas@users.noreply.github.com>
2022-04-20 15:07:16 -0400

    Delete Fundamentos de Programacion y Estructura de Datos directory

--- Fundamentos de Programacion y Estructura de Datos/Desafio I/emprendedor1.py
+++
@@ -1,9 +0,0 @@
-#Rodrigo Castillo
-
-P = float(input("Ingrese precio de suscripción: "))
-U = float(input("Ingrese numero de usuario: "))
-GT = float(input("Ingrese gasto total: "))
-
-utilidades = P * U - GT
-
-print("Las utilidades son de: ",utilidades)
